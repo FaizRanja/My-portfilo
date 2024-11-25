ec63f4417769ceade62a13c266b9836041f0d8c7
RanaFaiz <rfaiz6537@gmail.com>
2024-11-25 18:54:30 +0500

    send some data about my portfilo

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+<div className="sm:mx-auto sm:w-full sm:max-w-md">
+<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Login to your account
+        </h2>
+       </div>
+    </div> 
+   
+  )
+}
+
+export default App
+
+
